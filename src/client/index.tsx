2458dd01cf22192c68c3cde67e983d35370c61aa
Hunter Parcells <hparcells@icloud.com>
2020-05-23 13:53:45 -0400

    ga

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 
 import App from './components/App';
 
 import './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if(process.env.NODE_ENV === 'production') {
+  ReactGA.initialize('UA-134185568-10');
+  ReactGA.pageview('/');
+}
